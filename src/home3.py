c7078b9aad150b980ff4194f8920da03055dbf3d
BUDAPEST\Hanna_Tsyhanava <tsyhanava@tut.by>
2024-04-25 23:38:41 +0200

    home work 3.0

--- src/home3.py
+++ src/home3.py
@@ -1,3 +1,10 @@
+def main():
+    print('Hello, World!')
+
+
+if __name__ == '__main__':
+    main()
+
 import re
 
 def camel_to_snake(text):
@@ -8,6 +15,6 @@ def camel_to_snake(text):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 # Example
-camel_case_text = "CamelCaseExample"
+camel_case_text = "  tHis iz your homeWork, copy these Text to variable. You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph."
 snake_case_text = camel_to_snake(camel_case_text)
 print(snake_case_text)  # Output: camel_case_example
