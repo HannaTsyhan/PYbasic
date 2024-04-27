2a6e9bfb5f28cc56a24f9e822ee89574d9f55eb4
BUDAPEST\Hanna_Tsyhanava <tsyhanava@tut.by>
2024-04-27 23:35:31 +0200

    Home work 3

--- src/home3.py
+++ src/home3.py
@@ -1,20 +1,53 @@
-def main():
-    print('Hello, World!')
-
-
-if __name__ == '__main__':
-    main()
-
 import re
 
-def camel_to_snake(text):
-    # Use regular expression to find all capital letters
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
+if __name__ == '__main__':
 
-    # Convert the remaining capital letters to lowercase
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    initial_text = """
+homEwork:
+  tHis iz your homeWork, copy these Text to variable.
 
-# Example
-camel_case_text = "  tHis iz your homeWork, copy these Text to variable. You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph."
-snake_case_text = camel_to_snake(camel_case_text)
-print(snake_case_text)  # Output: camel_case_example
+  You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH 
+LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph. 
+
+  it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+
+  last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, 
+but ALL whitespaces. I got 87.
+"""
+
+lowercase_text = initial_text.lower()  # Normalize letter cases to lower case
+paragraphs: list = lowercase_text.split("\n\n")   # Split the text into paragraphs
+capitalized_paragraphs = []
+joined_text = []
+last_words = []
+
+for paragraph in paragraphs:  # Process paragraphs
+    capitalized_paragraph = paragraph.lstrip().capitalize().strip('\n')  # Capitalize the first letters in paragraphs
+    # print(capitalized_paragraph)
+    sentences = capitalized_paragraph.split(".")  # Split the paragraphs into sentences
+    sentences.pop()
+    # print(sentences)
+    capitalized_sentences = []
+
+    for sentence in sentences:  # Process sentences
+        capitalized_sentence = sentence.strip().capitalize() + ". "  # Capitalize sentences
+        words = sentence.split(" ")  # Get the last words
+        last_words.append(words[-1])
+        capitalized_sentences.append(capitalized_sentence)
+
+    joined_paragraph = "  " + "".join(f"{row}" for row in capitalized_sentences).rstrip()   # Join the sentences in paragraphs
+    capitalized_paragraphs.append(joined_paragraph)
+
+
+last_sentence = " ".join(last_words).capitalize() + "."  # Join last words to create the  sentence and add it the text
+capitalized_paragraphs[1] += " " + last_sentence
+
+joined_text = "".join(f"{parag}\n\n" for parag in capitalized_paragraphs)   # Join the paragraphs in text
+
+
+corrected_text = joined_text.replace(" iz ", " is ")   # Correct error iz -> is
+print(corrected_text)
+
+whitespace_pattern = re.compile(r'\s+')  # Calculate number of whitespaces
+whitespaces_count_corrected = len(whitespace_pattern.findall(corrected_text))
+print(whitespaces_count_corrected)
